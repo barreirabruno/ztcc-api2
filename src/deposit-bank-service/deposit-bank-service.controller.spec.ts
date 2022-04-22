f4d2eb8f12327fe1132c73a053a3db33d4ba3957
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-22 16:33:53 -0300

    test: ensure it rethrow if DepositBankService throws

--- src/deposit-bank-service/deposit-bank-service.controller.spec.ts
+++ src/deposit-bank-service/deposit-bank-service.controller.spec.ts
@@ -1,5 +1,6 @@
+import { HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { DepositBankServiceController } from './deposit-bank-service.controller';
+import { DepositBankService, DepositBankServiceController } from './deposit-bank-service.controller';
 
 const params = {
   "destination": {
@@ -15,9 +16,17 @@ const params = {
 describe('DepositBankServiceController', () => {
   let controller: DepositBankServiceController;
 
+  const mockDepositService = {
+    perform: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DepositBankServiceController],
+      providers: [{
+        provide: DepositBankService,
+        useValue: mockDepositService
+      }]
     }).compile();
 
     controller = module.get<DepositBankServiceController>(DepositBankServiceController);
@@ -31,5 +40,12 @@ describe('DepositBankServiceController', () => {
     const controllerSpy = jest.spyOn(controller, "postDepositTransactionAccount")
     controller.postDepositTransactionAccount(params)
     expect(controllerSpy).toHaveBeenCalledWith(params)
+  })
+
+  it('should rethrow if DepositBankService throws', async () => {
+    mockDepositService.perform.mockRejectedValueOnce(() => {
+      throw new Error("[ANY_ERROR]")
+    })
+    await expect(controller.postDepositTransactionAccount(params)).rejects.toThrow()
   })
 });
