552e3f48d2cbb622cd35f4013c78e27be19e46f2
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-22 18:12:16 -0300

    feat: add route paths to controller

--- src/deposit-bank-service/deposit-bank-service.controller.ts
+++ src/deposit-bank-service/deposit-bank-service.controller.ts
@@ -2,13 +2,13 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { DepositTransactionAccount } from 'src/application/domain/features/deposit-transaction-account.interface';
 import { DepositBankService } from './deposit-bank-service.service';
 
-@Controller('deposit-bank-service')
+@Controller('account')
 export class DepositBankServiceController {
   constructor(
     private readonly depositBankService: DepositBankService
   ) {}
 
-  @Post()
+  @Post('deposit')
   async postDepositTransactionAccount(@Body() httpRequest: DepositTransactionAccount.Input): Promise<DepositTransactionAccount.Output> {
     return this.depositBankService.perform(httpRequest)
   }
