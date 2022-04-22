7c5f5a972a54e5f745b58c8444929a50647db904
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-22 18:24:59 -0300

    refactor: remove mocks from test to a single file

--- test/unit/deposit-bank-service/deposit-bank-service.controller.spec.ts
+++ test/unit/deposit-bank-service/deposit-bank-service.controller.spec.ts
@@ -1,17 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { controllerParamsMock, controllerResponseMock } from '../../mocks/e2e';
 import { DepositBankServiceController } from '../../../src/deposit-bank-service/deposit-bank-service.controller';
 import { DepositBankService } from '../../../src/deposit-bank-service/deposit-bank-service.service';
-
-const params = {
-  "destination": {
-      "vatNumber": "0000000000"
-  },
-  "amount": {
-      "currency": "USD",
-      "value": 225.78
-  },
-  "description": "any_description_for_this_transfer"
-}
 
 describe('DepositBankServiceController', () => {
   let controller: DepositBankServiceController;
@@ -38,37 +28,20 @@ describe('DepositBankServiceController', () => {
 
   it('should call DepositBankServiceController with correct params', () => {
     const controllerSpy = jest.spyOn(controller, "postDepositTransactionAccount")
-    controller.postDepositTransactionAccount(params)
-    expect(controllerSpy).toHaveBeenCalledWith(params)
+    controller.postDepositTransactionAccount(controllerParamsMock)
+    expect(controllerSpy).toHaveBeenCalledWith(controllerParamsMock)
   })
 
   it('should rethrow if DepositBankService throws', async () => {
     mockDepositService.perform.mockRejectedValueOnce(() => {
       throw new Error("[ANY_ERROR]")
     })
-    await expect(controller.postDepositTransactionAccount(params)).rejects.toThrow()
+    await expect(controller.postDepositTransactionAccount(controllerParamsMock)).rejects.toThrow()
   })
 
   it('should return DepositBankService infos with success', async () => {
-    const mockServiceResponse = {
-      id: "any_transfer_id",
-      object: "transfer",
-      amount: {
-        currency: "BRL",
-        value: 250.78
-      },
-     created: 1405637071,
-     transactionAccountInfos: {
-       source: {
-        vatNumber: "00000000000"
-       },
-       destination: {
-         vatNumher: "00000000000"
-       }
-     }
-    }
-    mockDepositService.perform.mockResolvedValueOnce(mockServiceResponse)
-    const request = await controller.postDepositTransactionAccount(params)
-    expect(request).toEqual(mockServiceResponse)
+    mockDepositService.perform.mockResolvedValueOnce(controllerResponseMock)
+    const request = await controller.postDepositTransactionAccount(controllerParamsMock)
+    expect(request).toEqual(controllerResponseMock)
   })
 });
