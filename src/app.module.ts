7c4787fb924a714f06baa7171c604ffbcc2de202
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-22 18:13:31 -0300

    feat: import DepositBankServiceModule on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DepositBankServiceModule } from './deposit-bank-service/deposit-bank-service.module';
 
 @Module({
   imports: [DepositBankServiceModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
