22a36e63856a4bd530c78ac9175c1354979332fa
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-23 12:44:24 -0300

    feat: import TypeOrmModule on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DepositBankServiceModule } from './deposit-bank-service/deposit-bank-service.module';
 
 @Module({
-  imports: [DepositBankServiceModule],
+  imports: [DepositBankServiceModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
