e817d4e7950c37df645362ff64be74fdcbb61a53
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-24 13:13:47 -0300

    refactor: change params destructuring property

--- src/deposit-bank-service/deposit-bank-service.service.ts
+++ src/deposit-bank-service/deposit-bank-service.service.ts
@@ -13,16 +13,16 @@ export class DepositBankService implements DepositTransactionAccountInterface {
 
   async perform(params: DepositTransactionAccount.Input): Promise<DepositTransactionAccount.Output | HttpException> {
     try {
-      const {data} =  await this.transactionAccountRequest.execute(params.destination.vatNumber)
+      const {data} =  await this.transactionAccountRequest.execute(params.vatNumber)
       if(data === undefined) return new Error("Could not process the transaction by now. Please contact support")
       const { status } = data
       if(status !== 'available') return new Error("Could not process the transaction by now. Please contact support")
       const transaction = Object.assign({}, {
         transactionObject: BankServiceEnum.DPST,
-        currency: params.amount.currency,
-        value: params.amount.value,
-        source: params.destination.vatNumber,
-        destination: params.destination.vatNumber
+        currency: params.currency,
+        value: params.value,
+        source: params.vatNumber,
+        destination: params.vatNumber
       })
       const recordTransaction = await this.bankTransactionAccountRepository.executeBankTransaction(transaction)
       return recordTransaction
