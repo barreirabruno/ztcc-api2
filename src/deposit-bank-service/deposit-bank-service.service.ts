c93da9662ba3eaac6df596b473835a2378a04b75
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-23 23:17:42 -0300

    feat: call TransactionRepository

--- src/deposit-bank-service/deposit-bank-service.service.ts
+++ src/deposit-bank-service/deposit-bank-service.service.ts
@@ -3,11 +3,13 @@ import { CurrencyEnum } from '../application/domain/features/currency-enum-trans
 import { BankServiceEnum } from '../application/domain/features/objects-enum-transaction-account';
 import { DepositTransactionAccountInterface, DepositTransactionAccount } from '../application/domain/features/deposit-transaction-account.interface';
 import { RequestTransactionAccountApiService } from '../infra/http/request-transaction-account-api.service';
+import { TransactionRepository } from 'src/infra/database/repo/transaction-repository';
 
 @Injectable()
 export class DepositBankService implements DepositTransactionAccountInterface {
   constructor(
-    private readonly transactionAccountRequest: RequestTransactionAccountApiService
+    private readonly transactionAccountRequest: RequestTransactionAccountApiService,
+    private readonly bankTransactionAccountRepository: TransactionRepository
   ) {}
 
   executeTransaction() {
@@ -36,6 +38,15 @@ export class DepositBankService implements DepositTransactionAccountInterface {
       if(data === undefined) return new Error("Could not process the transaction by now. Please contact support")
       const { status } = data
       if(status !== 'available') return new Error("Could not process the transaction by now. Please contact support")
+      const transaction = Object.assign({}, {
+        transactionObject: BankServiceEnum.DPST,
+        currency: params.amount.currency,
+        value: params.amount.value,
+        source: params.destination.vatNumber,
+        destination: params.destination.vatNumber
+      })
+      console.log('[OBJECT TRANSACTION][NOT INSTANCE]: ', transaction)
+      this.bankTransactionAccountRepository.executeBankTransaction(transaction)
       return this.executeTransaction()
     } catch (error) {
       return new Error(error)
