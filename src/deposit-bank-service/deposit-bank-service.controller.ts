1e29f3ed1a941570f3ed528f98c36a120fc6d367
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-24 12:22:38 -0300

    refactor: add validation DTO and ExceptionFilter

--- src/deposit-bank-service/deposit-bank-service.controller.ts
+++ src/deposit-bank-service/deposit-bank-service.controller.ts
@@ -1,6 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { ArgumentsHost, Body, Catch, Controller, ExceptionFilter, HttpException, Post, UseFilters } from '@nestjs/common';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { Response } from 'express';
 import { DepositTransactionAccount } from '../application/domain/features/deposit-transaction-account.interface';
 import { DepositBankService } from './deposit-bank-service.service';
+
+@Catch(HttpException)
+class ExceptionErrorHandler implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>();
+    const exceptionResponse = exception.getResponse();
+    const status = exception.getStatus()
+
+    response.status(status).json(exceptionResponse)
+  }
+}
+
+class DestinationDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(11)
+  @MaxLength(11)
+  vatNumber: string
+}
+
+class DepositDTO {
+  destination: DestinationDTO
+  amount: {
+    currency: string,
+    value: number
+  }
+}
 
 @Controller('account')
 export class DepositBankServiceController {
@@ -9,7 +39,10 @@ export class DepositBankServiceController {
   ) {}
 
   @Post('deposit')
-  async postDepositTransactionAccount(@Body() httpRequest: DepositTransactionAccount.Input): Promise<DepositTransactionAccount.Output> {
-    return this.depositBankService.perform(httpRequest)
+  @UseFilters(ExceptionErrorHandler)
+  async postDepositTransactionAccount(@Body() httpRequest: DepositDTO): Promise<DepositTransactionAccount.Output> {
+    const service = await this.depositBankService.perform(httpRequest)
+    console.log(service)
+    return service
   }
 }
