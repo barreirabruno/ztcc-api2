89044ece42634dc5d66fc47c2ce94db3c95b7d55
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-26 17:12:41 -0300

    refactor: update mock imports on controllers test

--- test/unit/deposit-bank-service/deposit-bank-service.controller.spec.ts
+++ test/unit/deposit-bank-service/deposit-bank-service.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { controllerParamsMock, controllerResponseMock } from '../../mocks/e2e';
+import { controllerParamsMockTRSF, controllerResponseMockTRS } from '../../mocks/e2e';
 import { BankServicesAPIController } from '../../../src/bank-services-api/bank-services-api.controller';
 import { BankServicesAPIService } from '../../../src/bank-services-api/bank-services-api.service';
 
@@ -28,20 +28,20 @@ describe('DepositBankServiceController', () => {
 
   it('should call DepositBankServiceController with correct params', () => {
     const controllerSpy = jest.spyOn(controller, "postDepositTransactionAccount")
-    controller.postDepositTransactionAccount(controllerParamsMock)
-    expect(controllerSpy).toHaveBeenCalledWith(controllerParamsMock)
+    controller.postDepositTransactionAccount(controllerParamsMockTRSF)
+    expect(controllerSpy).toHaveBeenCalledWith(controllerParamsMockTRSF)
   })
 
   it('should rethrow if DepositBankService throws', async () => {
     mockDepositService.perform.mockRejectedValueOnce(() => {
       throw new Error("[ANY_ERROR]")
     })
-    await expect(controller.postDepositTransactionAccount(controllerParamsMock)).rejects.toThrow()
+    await expect(controller.postDepositTransactionAccount(controllerParamsMockTRSF)).rejects.toThrow()
   })
 
   it('should return DepositBankService infos with success', async () => {
-    mockDepositService.perform.mockResolvedValueOnce(controllerResponseMock)
-    const request = await controller.postDepositTransactionAccount(controllerParamsMock)
-    expect(request).toEqual(controllerResponseMock)
+    mockDepositService.perform.mockResolvedValueOnce(controllerResponseMockTRS)
+    const request = await controller.postDepositTransactionAccount(controllerParamsMockTRSF)
+    expect(request).toEqual(controllerResponseMockTRS)
   })
 });
