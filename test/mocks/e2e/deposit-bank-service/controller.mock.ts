7c65a0e41f8235f73a483d2897d21a8b78014c08
brunobarreira <brunobarreirasilva@gmail.com>
2022-04-25 22:55:57 -0300

    refactor: update mocks

--- test/mocks/e2e/deposit-bank-service/controller.mock.ts
+++ test/mocks/e2e/deposit-bank-service/controller.mock.ts
@@ -1,12 +1,29 @@
-export const controllerParamsMock = {
+export const controllerParamsMockTRSF = {
     "vatNumberSource": "0000000000",
     "vatNumberDestination": "11111111111",
     "currency": "USD",
     "value": 225.00
 }
 
-export const controllerResponseMock = {
+export const controllerParamsMockDPTS = {
+    "vatNumberSource": "0000000000",
+    "currency": "USD",
+    "value": 225.00
+}
+
+export const controllerResponseMockDPST = {
     "transactionObject": "deposit",
+    "currency": "BRL",
+    "value": "66.13",
+    "source": "80670530816",
+    "destination": "80670530816",
+    "id": 24,
+    "transactionId": "c2b5d1da-cd30-4c8a-b356-dbe1226c7b4f",
+    "createdAt": "2022-04-24T03:04:52.272Z"
+}
+
+export const controllerResponseMockTRS = {
+    "transactionObject": "transfer",
     "currency": "BRL",
     "value": "66.13",
     "source": "80670530816",
